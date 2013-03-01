ed8c9b8bfcd61d8d2330d077c6aec75cfda3e2e9
Laurence LeBron <larrylebron@gmail.com>
2013-03-01 15:45:47 -0800

    Adding the unity files

---
+++ Assets/fooScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class fooScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("tennnnse!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
