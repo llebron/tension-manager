6a1400bd244e745186010239ad6dd3b6661b057a
Laurence LeBron <larrylebron@gmail.com>
2013-03-01 16:05:35 -0800

    Added Tension Manager

--- Assets/scripts/TensionManager.cs
+++ Assets/scripts/TensionManager.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
 using System.Collections;
 
-public class fooScript : MonoBehaviour {
+public class TensionManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("tennnnse!");
+		Debug.Log ("start tension manager");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log ("update");
 	}
 }
