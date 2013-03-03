101a653e9403b4474ab7ecc553249054f35c8f18
Laurence LeBron <larrylebron@gmail.com>
2013-03-03 11:40:47 -0800

    Added GUI stuff.

--- Assets/scripts/TensionManager.cs
+++ Assets/scripts/TensionManager.cs
@@ -5,11 +5,9 @@ public class TensionManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("start tension manager");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ("update");
 	}
 }
